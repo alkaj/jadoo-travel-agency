998e68fc41b40afd9c5af7bebf3698e81df894b5
Junior Kamla <alkaj09@gmail.com>
2025-04-06 11:01:42 +0000

    Adding shadows to the destination card

--- lib/widgets/destinations.dart
+++ lib/widgets/destinations.dart
@@ -10,36 +10,50 @@ Widget DestinationCard({
   return Container(
     width: 250,
     decoration: BoxDecoration(
-      border: Border.all(color: greyColor.withAlpha(10), width: 2),
-      borderRadius: BorderRadius.circular(16),
+      // border: Border.all(color: greyColor.withAlpha(10), width: 2),
+      // borderRadius: BorderRadius.circular(16),
+      boxShadow: [
+        BoxShadow(
+          color: greyColor.withAlpha(15),
+          blurRadius: 20,
+          offset: Offset(0, 6),
+        ),
+      ],
     ),
     child: Stack(
       alignment: Alignment.bottomCenter,
-      children: [Image.asset(
-      'assets/images/destinations/$image.png',
-      width: 250,
-      fit: BoxFit.cover,
+      children: [
+        Image.asset(
+          'assets/images/destinations/$image.png',
+          width: 250,
+          fit: BoxFit.cover,
+        ),
+        Container(
+          width: 250,
+          color: whiteColor,
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 8,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text(name), Text(price)],
+              ),
+              Row(
+                spacing: 16,
+                children: [
+                  Image.asset(
+                    'assets/images/destinations/navigation.png',
+                    width: 16,
+                  ),
+                  Text(duration),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     ),
-    Container(
-      width: 250,
-      color: whiteColor,
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 8,
-        children: [Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        Text(name),
-        Text(price),
-      ],),
-      Row(
-        spacing: 16,
-        children: [
-        Image.asset('assets/images/destinations/navigation.png', width: 16),
-        Text(duration),],),
-      ],),
-    ),
-    ],),
   );
 }
